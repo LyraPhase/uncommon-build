9fd18f8fdf954236ab061072caf879fa5a9e3048
James Cuzella <james.cuzella@lyraphase.com>
2022-04-22 15:08:05 -0600

    Add uninstall-common-build target & easter egg stylish text

--- build-aux/uncommon-targets.mk
+++ build-aux/uncommon-targets.mk
@@ -4,11 +4,20 @@ $(build_stamps_dir): ## no-help
 
 .PHONY: update-uncommon-build
 update-uncommon-build: ## Update to latest $(COMMON_BUILD_REF). Default: origin/master
-	@echo "$(c_red)Updating '.uncommon-build' and '.gitmodules'$(c_reset)" >&2
+	@echo -e "$(c_bg_black)⚙️  🔧🫲 (🌀_🔍)🫴  $(c_reset)$(c_b_cyan) Updating '.uncommon-build' and '.gitmodules'$(c_reset)" >&2
 	@# Ensure `.uncommon-build` is updated and committed in .gitmodules
 	cd .uncommon-build && git fetch origin && git checkout $(UNCOMMON_BUILD_REF) && git pull
 	git add .uncommon-build .gitmodules && \
-	  echo git commit -m 'Updating .uncommon-build submodule to $(shell git rev-parse $(UNCOMMON_BUILD_REF))'
+	  git commit -m 'Updating .uncommon-build submodule to $(shell git rev-parse $(UNCOMMON_BUILD_REF))'
+
+.PHONY: update-uncommon-build
+uninstall-uncommon-build: ## Uninstall .uncommon-build submodule
+	@echo -e "$(c_bg_black)🗑️  📃🫲 (🌀_🔍)🫴  $(c_reset)$(c_b_hi_red) Uninstalling '.uncommon-build' and '.gitmodules'$(c_reset)" >&2
+	git submodule deinit -f .uncommon-build
+	git rm .gitmodules && \
+	  git commit -m 'Uninstalling .uncommon-build submodule'
+	rm -rf .git/modules/.uncommon-build/
+	@echo -e "$(c_bg_black)🧙🪄💥🫠$(c_reset) $(c_hi_green)Oh... what a world, what a world$(c_reset)"
 
 clean::
 	rm -rf $(build_stamps_dir)
