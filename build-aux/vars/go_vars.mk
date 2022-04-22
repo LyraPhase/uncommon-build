bbff40655217b7e1d64d04a770573755e1eb27d0
James Cuzella <james.cuzella@lyraphase.com>
2022-04-22 02:47:56 -0600

    Consolidate test reports, auto-detect mocks, test data & dependencies

--- build-aux/vars/go_vars.mk
+++ build-aux/vars/go_vars.mk
@@ -13,8 +13,8 @@ BUILD_TARGET ?= $(build_stamps_dir)/built
 # Default dependencies. If your project needs to modify the dependencies for a default target, replace these values in your project's `Makefile`
 PACKAGE_DEPENDENCIES ?= $(BIN) Dockerfile | $(DOCKER_CONFIG)/config.json
 
-TEST_GO    ?= $(GENERATED_OUT)/test-report.txt
-TEST_XUNIT ?= $(GENERATED_OUT)/test-report.xml
+TEST_GO    ?= $(build_stamps_dir)/test-report.txt
+TEST_XUNIT ?= $(build_stamps_dir)/test-report.xml
 
 BUILD_IMAGE ?= $(REGISTRY)/golang:alpine3.15
 
@@ -22,11 +22,12 @@ BUILD_IMAGE ?= $(REGISTRY)/golang:alpine3.15
 GOLANG_REPO           ?= $(GIT_REPOHOST)/$(GIT_REPOUSER)/$(REPO_NAME)
 CONTAINER_SOURCE_PATH ?= /go/src/$(GOLANG_REPO)
 
-TEST_DEPENDENCIES ?= $(shell find . -type f -name '*.go') $(EASY_JSON_FILES) $(MOCK_DIRS) | $(GENERATED_OUT)
-
 all_src_dirs = $(shell find $(top_srcdir)/ -type d)
 
+MOCK_DIRS  ?= $(shell find $(top_srcdir) -iname '*.go' -exec grep -l 'package mock' '{}'  \; | xargs -0 -n1 -I{} bash -c 'basename "$$(dirname "{}")"')
+TEST_DATA  ?= testdata
 TEST_FILES ?= $(foreach d,$(all_src_dirs),$(wildcard $(d)/*_test.go))
+TEST_DEPENDENCIES ?= $(shell find . -type f -name '*.go') $(MOCK_DIRS) | $(build_stamps_dir)
 
 GO_DEPENDENCIES ?= go.sum
 
