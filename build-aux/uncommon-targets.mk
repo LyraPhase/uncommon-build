0e4eca4705ce5e6cd0b1ac6ce39528cd6260d731
James Cuzella <james.cuzella@lyraphase.com>
2022-04-22 03:55:02 -0600

    Fix UNCOMMON_BUILD_REF

--- build-aux/uncommon-targets.mk
+++ build-aux/uncommon-targets.mk
@@ -6,9 +6,9 @@ $(build_stamps_dir): ## no-help
 update-uncommon-build: ## Update to latest $(COMMON_BUILD_REF). Default: origin/master
 	@echo "$(c_red)Updating '.uncommon-build' and '.gitmodules'$(c_reset)" >&2
 	@# Ensure `.uncommon-build` is updated and committed in .gitmodules
-	cd .uncommon-build && git fetch origin && git checkout $(COMMON_BUILD_REF) && git pull
+	cd .uncommon-build && git fetch origin && git checkout $(UNCOMMON_BUILD_REF) && git pull
 	git add .uncommon-build .gitmodules && \
-	  echo git commit -m 'Updating .uncommon-build submodule to $(shell git rev-parse $(COMMON_BUILD_REF))'
+	  echo git commit -m 'Updating .uncommon-build submodule to $(shell git rev-parse $(UNCOMMON_BUILD_REF))'
 
 clean::
 	rm -rf $(build_stamps_dir)
