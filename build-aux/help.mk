f82b5c018f2ff3cea8ffebcd29c13f986fbf955d
James Cuzella <james.cuzella@lyraphase.com>
2022-04-22 17:15:40 -0600

    help: Switch to use ansicolor macros

--- build-aux/help.mk
+++ build-aux/help.mk
@@ -3,4 +3,4 @@ DEFAULT_GOAL: help ## no-help
 .PHONY: help
 
 help: ## Shows this generated help info for Makefile targets
-	@grep -E '^[a-zA-Z_-]+:' $(MAKEFILE_LIST) | awk '{ c=split($$0,resultArr,/:+/) ; if ( !(resultArr[c-1] in targets) ) { if ( /:.*##/ ) { if ( ! /no-help/ ) { sub(/^.*## ?/," ",resultArr[c]); targets[resultArr[c-1]] = resultArr[c]; } } else { targets[resultArr[c-1]] = "" } } } END { for (target in targets) { printf "\033[36m%-30s\033[0m %s\n", target, targets[target] } }' | sort
+	@grep -E '^[a-zA-Z_-]+:' $(MAKEFILE_LIST) | awk '{ c=split($$0,resultArr,/:+/) ; if ( !(resultArr[c-1] in targets) ) { if ( /:.*##/ ) { if ( ! /no-help/ ) { sub(/^.*## ?/," ",resultArr[c]); targets[resultArr[c-1]] = resultArr[c]; } } else { targets[resultArr[c-1]] = "" } } } END { for (target in targets) { printf "$(c_cyan)%-30s$(c_reset) %s\n", target, targets[target] } }' | sort
