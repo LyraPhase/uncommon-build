ab470106eeb5a3d341212329f601e49558b079c7
James Cuzella <james.cuzella@lyraphase.com>
2022-04-22 02:03:08 -0600

    Add ANSI color vars

---
+++ build-aux/vars/ansicolor_vars.mk
@@ -0,0 +1,70 @@
+# ANSI Colors
+c_reset         := \033[0m
+c_black         := \033[0;30m
+c_red           := \033[0;31m
+c_green         := \033[0;32m
+c_yellow        := \033[0;33m
+c_blue          := \033[0;34m
+c_magenta       := \033[0;35m
+c_cyan          := \033[0;36m
+c_white         := \033[0;37m
+
+# Bold colors
+c_b_black       := \033[1;30m
+c_b_red         := \033[1;31m
+c_b_green       := \033[1;32m
+c_b_yellow      := \033[1;33m
+c_b_blue        := \033[1;34m
+c_b_magenta     := \033[1;35m
+c_b_cyan        := \033[1;36m
+c_b_white       := \033[1;37m
+
+# Underline text
+u_black         := \033[4;30m
+u_red           := \033[4;31m
+u_green         := \033[4;32m
+u_yellow        := \033[4;33m
+u_blue          := \033[4;34m
+u_magenta       := \033[4;35m
+u_cyan          := \033[4;36m
+u_white         := \033[4;37m
+
+# Background colors
+c_bg_black      := \033[40m
+c_bg_red        := \033[41m
+c_bg_green      := \033[42m
+c_bg_yellow     := \033[43m
+c_bg_blue       := \033[44m
+c_bg_magenta    := \033[45m
+c_bg_cyan       := \033[46m
+c_bg_white      := \033[47m
+
+# High intensty background
+c_hi_bg_black   := \033[0;100m
+c_hi_bg_red     := \033[0;101m
+c_hi_bg_green   := \033[0;102m
+c_hi_bg_yellow  := \033[0;103m
+c_hi_bg_blue    := \033[0;104m
+c_hi_bg_magenta := \033[0;105m
+c_hi_bg_cyan    := \033[0;106m
+c_hi_bg_white   := \033[0;107m
+
+# High intensty text colors
+c_hi_black      := \033[0;90m
+c_hi_red        := \033[0;91m
+c_hi_green      := \033[0;92m
+c_hi_yellow     := \033[0;93m
+c_hi_blue       := \033[0;94m
+c_hi_magenta    := \033[0;95m
+c_hi_cyan       := \033[0;96m
+c_hi_white      := \033[0;97m
+
+# Bold high intensity text colors
+c_b_hi_black    := \033[1;90m
+c_b_hi_red      := \033[1;91m
+c_b_hi_green    := \033[1;92m
+c_b_hi_yellow   := \033[1;93m
+c_b_hi_blue     := \033[1;94m
+c_b_hi_magenta  := \033[1;95m
+c_b_hi_cyan     := \033[1;96m
+c_b_hi_white    := \033[1;97m
